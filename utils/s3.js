5af40d2a439a476a53c0fe85249b8c0565579afc
saka360ke-lang <saka360ke@gmail.com>
2025-10-26 09:43:13 +0300

    Force commit all changes

--- utils/s3.js
+++ utils/s3.js
@@ -1,57 +1,39 @@
 // utils/s3.js
-const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
-const {
-  AWS_REGION,
-  AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY,
-  AWS_SESSION_TOKEN,
-  S3_BUCKET,
-} = process.env;
+const REGION = process.env.S3_REGION;
+const BUCKET = process.env.S3_BUCKET;
 
-function getS3() {
-  if (!AWS_REGION) throw new Error("Missing AWS_REGION");
-  if (!S3_BUCKET) throw new Error("Missing S3_BUCKET");
-
-  const credentials =
-    AWS_ACCESS_KEY_ID && AWS_SECRET_ACCESS_KEY
-      ? {
-          accessKeyId: AWS_ACCESS_KEY_ID,
-          secretAccessKey: AWS_SECRET_ACCESS_KEY,
-          sessionToken: AWS_SESSION_TOKEN || undefined,
-        }
-      : undefined;
-
-  return new S3Client({ region: AWS_REGION, credentials });
-}
-
-function debugAws() {
-  return {
-    region: AWS_REGION || null,
-    bucket: S3_BUCKET || null,
-    accessKeyIdPresent: Boolean(AWS_ACCESS_KEY_ID),
-    secretPresent: Boolean(AWS_SECRET_ACCESS_KEY),
-    sessionTokenPresent: Boolean(AWS_SESSION_TOKEN),
-  };
-}
-
-async function getPresignedGetUrl({ key, expiresInSec = 300 }) {
-  if (!key) throw new Error("key is required");
-  const s3 = getS3();
-  const cmd = new GetObjectCommand({ Bucket: S3_BUCKET, Key: key });
-  return getSignedUrl(s3, cmd, { expiresIn: expiresInSec });
-}
-
-async function getPresignedPutUrl({ key, contentType = "application/octet-stream", expiresInSec = 300 }) {
-  if (!key) throw new Error("key is required");
-  const s3 = getS3();
-  const cmd = new PutObjectCommand({ Bucket: S3_BUCKET, Key: key, ContentType: contentType });
-  return getSignedUrl(s3, cmd, { expiresIn: expiresInSec });
-}
-
-module.exports = {
-  debugAws,
-  getPresignedGetUrl,
-  getPresignedPutUrl,
+const creds = {
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  sessionToken: process.env.S3_SESSION_TOKEN || undefined, // <-- support STS if present
 };
+
+const s3 = new S3Client({
+  region: REGION,
+  credentials: creds,
+});
+
+function publicUrl(key) {
+  return `https://${BUCKET}.s3.${REGION}.amazonaws.com/${encodeURIComponent(key)}`;
+}
+
+async function signPutUrl({ key, contentType, expiresIn = 900 }) {
+  const cmd = new PutObjectCommand({
+    Bucket: BUCKET,
+    Key: key,
+    ContentType: contentType || "application/octet-stream",
+  });
+  const url = await getSignedUrl(s3, cmd, { expiresIn });
+  return { url, key, contentType: contentType || "application/octet-stream", publicUrl: publicUrl(key) };
+}
+
+async function signGetUrl({ key, expiresIn = 300 }) {
+  const cmd = new GetObjectCommand({ Bucket: BUCKET, Key: key });
+  const url = await getSignedUrl(s3, cmd, { expiresIn });
+  return { url, key, expiresInSec: expiresIn, method: "GET" };
+}
+
+module.exports = { signPutUrl, signGetUrl, publicUrl };
